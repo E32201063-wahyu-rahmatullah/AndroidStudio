64f93084740abeea9a0f5e96177b1d8fd394f2df
Wahyu Rahmatullah <wrahmatullah105@gmail.com>
2021-09-30 00:13:05 +0700

    Pertemuan 1-2

---
+++ Minggu ke 3/ConstrainLayout/app/src/main/java/com/e32201063/constrainlayout/ConstraintLayout.java
@@ -0,0 +1,4 @@
+package com.e32201063.constrainlayout;
+
+public class ConstraintLayout {
+}
---
+++ Minggu ke 3/Tabel_Layout/app/src/main/java/com/e32201063/tabel_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.e32201063.tabel_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
